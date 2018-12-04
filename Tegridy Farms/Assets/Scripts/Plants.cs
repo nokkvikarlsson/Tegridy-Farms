7e3e57a96673ea0f27008003453a42af10968081
Atli Gislason <atlig17@ru.is>
2018-12-04 10:20:57 +0000

    edited the scripts

---
+++ Tegridy Farms/Assets/Scripts/Plants.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plants : MonoBehaviour {
+
+	public class Plant {
+		public Plant(string _type, int _price, double _growthrate, int _sellvalue, double _suspicion){
+			this.type = _type;
+			this.price = _price;
+			this.growthrate = _growthrate; //progress made from 0 to 1, per game hour
+			this.sellvalue = _sellvalue;
+			this.suspicion = _suspicion; //suspicion added for every unlaundered dollar earned. suspicion is from 0 to 1.
+		}
+		public string type;
+		public int price;
+		public double growthrate;
+		public int sellvalue;
+		public double suspicion;
+	}
+
+	public List<Plant> AllPlants;
+
+	// Use this for initialization
+	void Start() {
+		AllPlants.Add(new Plant("Cannabis", 30, 0.25, 50, 0.05));
+		AllPlants.Add(new Plant("Tobacco", 10, 0.2, 15, 0));
+		AllPlants.Add(new Plant("Hash", 50, 0.18, 80, 0.05));
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		
+	}
+}
--- Tegridy Farms/Assets/Scripts/Plot.cs
+++ Tegridy Farms/Assets/Scripts/Plot.cs
@@ -2,15 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plot.cs : MonoBehaviour {
+public class Plot : MonoBehaviour {
+
+	public string type; //string of type of plant or building. i.e. "Cannabis"
+	public double growth; //double ranging from 0 to 1, representing growth progress
+						  // 0 is newly planted, 1 is ready for harvest.
+	
+
+	private GameController _gameController;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		
+	}
+
+	void Awake() {
+		_gameController = FindObjectOfType<GameController>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 		
 	}
 }
