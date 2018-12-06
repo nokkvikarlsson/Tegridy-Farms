01fcddae42db1c9faa1d67326813a38100729751
nokkvikarlsson <nokkvi17@ru.is>
2018-12-06 23:00:13 +0000

    Expand now updates the tilemap

--- Tegridy Farms/Assets/Scripts/Expand.cs
+++ Tegridy Farms/Assets/Scripts/Expand.cs
@@ -17,6 +17,9 @@ public class Expand : MonoBehaviour {
     {
         _plotSize2x2 = true;
         _plotSize3x3 = false;
+        _field2x2 = GameObject.Find("Tilemap_field_2x2");
+        _field3x3 = GameObject.Find("Tilemap_field_3x3");
+        _field4x4 = GameObject.Find("Tilemap_field_4x4");
 
     }
 
@@ -24,9 +27,7 @@ public class Expand : MonoBehaviour {
     void Start () 
     {
         _plots = GameObject.FindGameObjectsWithTag("plot");
-        _field2x2 = GameObject.Find("Tilemap_field_2x2");
-        _field3x3 = GameObject.Find("Tilemap_field_3x3");
-        _field4x4 = GameObject.Find("Tilemap_field_4x4");
+
     }
 	
 	// Update is called once per frame
@@ -42,8 +43,8 @@ public class Expand : MonoBehaviour {
         if(_plotSize2x2)
         {
 
-            _field2x2.SetActive(false);
-            _field3x3.SetActive(true);
+            _field2x2.GetComponent<Renderer>().enabled = false;
+            _field3x3.GetComponent<Renderer>().enabled = true;
 
             //3x3 plots
             _plots[2].GetComponent<SpriteRenderer>().enabled = true;
@@ -67,8 +68,8 @@ public class Expand : MonoBehaviour {
         else if(_plotSize3x3)
         {
 
-            _field3x3.SetActive(false);
-            _field4x4.SetActive(true);
+            _field3x3.GetComponent<Renderer>().enabled = false;
+            _field4x4.GetComponent<Renderer>().enabled = true;
 
             _plots[3].GetComponent<SpriteRenderer>().enabled = true;
             _plots[3].GetComponent<BoxCollider2D>().enabled = true;
