78303d7c2023e08f896fbf2ca7edc13bf8612827
nokkvikarlsson <nokkvi17@ru.is>
2018-12-10 17:49:19 +0000

    Nearly finished with saving high scores

--- Tegridy Farms/Assets/Scripts/DisplayScore.cs
+++ Tegridy Farms/Assets/Scripts/DisplayScore.cs
@@ -27,15 +27,50 @@ public class DisplayScore : MonoBehaviour {
     public void Display () {
 
         int day = _gameController.getDayCounter();
-        int money = _gameController.money;
-        int totalScore = day * money;
+        int totalMoney = _gameController.totalMoneyEarned;
+        int totalScore = day * totalMoney;
 
         if(totalScore < 0)
         {
             totalScore = 0;
         }
 
-        _totalMoneyText.text = "Total money earned:" + money + "$\n" + "Total days lasted: " + day.ToString() + "\nTotal Score:" + totalScore;
+        //If the totalScore is higher than the current highscore then save that as the highscore.
+        if (totalScore >= PlayerPrefs.GetInt("HighestScore", 0))
+        {
+            Debug.Log("HALLO A");
+            PlayerPrefs.SetInt("HighestScore", totalScore);
+            _totalMoneyText.text = "Total money earned:" + totalMoney + "$\n" + "Total days lasted: " + day.ToString() + "\nNew highscore:" + totalScore;
+
+
+            //Cheks if the score is lower then the seat above and highest then the current seat in the scoreboard.
+            if(totalScore >= PlayerPrefs.GetInt("2HighestScore", 0) && totalScore < PlayerPrefs.GetInt("HighestScore"))
+            {
+                PlayerPrefs.SetInt("2HighestScore", totalScore);
+            }
+            else if(totalScore >= PlayerPrefs.GetInt("3HighestScore", 0) && totalScore < PlayerPrefs.GetInt("2HighestScore"))
+            {
+                PlayerPrefs.SetInt("3HighestScore", totalScore);
+            }
+            else if(totalScore >= PlayerPrefs.GetInt("4HighestScore", 0) && totalScore < PlayerPrefs.GetInt("3HighestScore"))
+            {
+                PlayerPrefs.SetInt("4HighestScore", totalScore);
+            }
+            else if(totalScore >= PlayerPrefs.GetInt("5HighestScore", 0) && totalScore < PlayerPrefs.GetInt("4HighestScore"))
+            {
+                PlayerPrefs.SetInt("5HighestScore", totalScore);
+            }
+
+        }
+        //If the total score is lower than the highscore then display the score and the current highscore.
+        else
+        {
+            Debug.Log("HALLO B");
+            int currentHighscore = PlayerPrefs.GetInt("HighestScore", 0);
+            _totalMoneyText.text = "Total money earned:" + totalMoney + "$\n" + "Total days lasted: " 
+                                    + day.ToString() + "\nTotal Score:" + totalScore + "\nCurrent highscore" + currentHighscore;
+        }
+
     }
 
 }
---
+++ Tegridy Farms/Assets/Scripts/DisplayScoreList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayScoreList : MonoBehaviour {
+
+
+    private TextMeshProUGUI _highScoreText;
+
+    // Use this for initialization
+    void Awake () {
+        _highScoreText = GetComponent<TextMeshProUGUI>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        int highestScore = PlayerPrefs.GetInt("HighestScore", 0);
+        int highestScore2nd = PlayerPrefs.GetInt("2HighestScore", 0);
+        int highestScore3rd = PlayerPrefs.GetInt("3HighestScore", 0);
+        int highestScore4rd = PlayerPrefs.GetInt("4HighestScore", 0);
+        int highestScore5th = PlayerPrefs.GetInt("5HighestScore", 0);
+
+
+        _highScoreText.text = "1. " + highestScore + 
+                            "\n2. " + highestScore2nd +
+                            "\n3. " + highestScore3rd +
+                            "\n4. " + highestScore4rd +
+                            "\n5. " + highestScore5th;
+
+	}
+}
