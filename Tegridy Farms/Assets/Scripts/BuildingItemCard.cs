0054d454069ed110cd23052ba98d730af1b5241b
Atli Gislason <atlig17@ru.is>
2018-12-10 19:10:08 +0000

    building item cards

--- Tegridy Farms/Assets/Scripts/BuildingItemCard.cs
+++ Tegridy Farms/Assets/Scripts/BuildingItemCard.cs
@@ -1,21 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class BuildingItemCard : MonoBehaviour {
+public class BuildingItemCard : MonoBehaviour
+{
+	public int shopItemIndex;
+	private Text _itemCardTitleText;
+	private GameController _gameController;
+	private ShopItems _shopItems;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake()
+	{
+		_gameController = FindObjectOfType<GameController>();
+		_shopItems = FindObjectOfType<ShopItems>();
+		GameObject itemCardTitle = gameObject.transform.GetChild(0).gameObject;
+		_itemCardTitleText = itemCardTitle.GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		
 	}
 
 	public void OnMouseClick()
 	{
-		
+
 	}
 }
